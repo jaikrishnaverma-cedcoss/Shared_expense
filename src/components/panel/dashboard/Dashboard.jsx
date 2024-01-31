c7b719fdbb6a12f555a9cfa50fa295c8b1bbdc2b
jaikrishnaverma-dev <jaikrishnaverma@gmail.com>
2024-01-31 20:37:01 +0530

    ui

--- src/components/panel/dashboard/Dashboard.jsx
+++ src/components/panel/dashboard/Dashboard.jsx
@@ -38,7 +38,7 @@ const Dashboard = () => {
       </Grid>
       {/* Recent Orders */}
       <Grid item xs={12}>
-        <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+        <Paper sx={{ p: 0, display: "flex", flexDirection: "column" }}>
           <Orders />
         </Paper>
       </Grid>
